c6c63a08db6cb41de9ae5c2652d3d0e7180c1ec6
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-06-28 22:44:53 +0530

    [feat]: add useSyncExternalStore hook in useStateSelector hook for getting state

--- src/Context/StoreContext/useStoreState.tsx
+++ src/Context/StoreContext/useStoreState.tsx
@@ -1,28 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useSyncExternalStore } from 'react'
 import { useStoreCtx } from './StoreContext'
 import { StoreCtxState } from './getStoreData'
 
 export function useStateSelector<SelectorOutput>(selector: (state: StoreCtxState) => SelectorOutput): SelectorOutput {
    const Store = useStoreCtx()
-
    if (!Store) throw new Error('no form store found.')
 
-   const [state, setState] = useState(() => selector(Store.get()))
-
-   useEffect(() => {
-      return Store.observe(() => setState(() => selector(Store.get())))
-   }, [])
-
-   // returning state
+   const state = useSyncExternalStore(Store.observe, () => selector(Store.get()))
    return state
 }
+
 export function useStateDispatch() {
    const FormStore = useStoreCtx()
-
    if (!FormStore) throw new Error('no form store found.')
 
    return FormStore.set
 }
+
 export function useContextActions() {
    const FormStore = useStoreCtx()
    if (!FormStore) throw new Error('no form store found.')
